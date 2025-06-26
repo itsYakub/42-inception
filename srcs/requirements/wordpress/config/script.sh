6a52432e28c5c2f3bea71649f70a04bd8ae08f3a
Jakub Oleksiak <yakubofficialmail@gmail.com>
2025-06-26 08:27:37 +0000

    Ready for eval
    I think at least

--- srcs/requirements/wordpress/config/script.sh
+++ srcs/requirements/wordpress/config/script.sh
@@ -6,8 +6,10 @@ mkdir -p /var/www/html
 mkdir -p /run/php
 cd /var/www/html
 
+# downloading the wordpress onto this machine
 wp-cli.phar core download --allow-root
 
+# creating configuration files at the default location of wordpress
 wp-cli.phar config create \
 	--dbname=$MARIADB_DBNAME \
 	--dbuser=$MARIADB_ADMIN_USER \
@@ -16,6 +18,7 @@ wp-cli.phar config create \
 	--allow-root \
 	--skip-check
 
+# installing the wordpress page
 wp-cli.phar core install \
 	--url=$WP_DOMAIN \
 	--title=$WP_TITLE \
@@ -25,16 +28,25 @@ wp-cli.phar core install \
 	--skip-email \
 	--allow-root
 
-wp-cli.phar user create $WP_USER $WP_EMAIL \
+# administrator role
+wp-cli.phar user create $WP_ADMIN $WP_ADMIN_EMAIL \
 	--role=administrator \
+	--user_pass=$WP_ADMIN_PSWD \
+	--allow-root
+
+# regular user
+wp-cli.phar user create $WP_USER $WP_EMAIL \
 	--user_pass=$WP_PSWD \
 	--allow-root
 
+# configuring the config
 wp-cli.phar config set FORCE_SSS_ADMIN 'false' --allow-root
 
+# setting up a theme
 wp-cli.phar theme install astra --activate --allow-root
 wp-cli.phar plugin update --all --allow-root
 
 chmod 777 /var/www/html/wp-content
 
+# launching the php runtime
 /usr/sbin/php-fpm7.4 -F
